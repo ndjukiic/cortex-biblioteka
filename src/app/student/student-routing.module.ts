c4b23bd7562ba96fa5dba77e237645a0e0b11cfd
Ivan Backovic <ng1v4n85@gmail.com>
2023-09-04 13:22:36 +0200

    added student datail component

--- src/app/student/student-routing.module.ts
+++ src/app/student/student-routing.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { StudentListComponent } from './components/student-list/student-list.component';
+import { StudentDetailComponent } from './components/student-detail/student-detail.component';
+import { StudentTabDetailComponent } from './components/student-detail/student-tab-detail/student-tab-detail.component';
 
 const routes = [
   {
     path: '',
     component: StudentListComponent,
   },
-  
-];
+  {
+    path: ':id',
+    component: StudentDetailComponent,
+    children: [
+      {
+        path: 'details',
+        component: StudentTabDetailComponent,
+      },
+    ],
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -2,17 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentListComponent } from './components/student-list/student-list.component';
 import { StudentRoutingModule } from './student-routing.module';
+import { StudentDetailComponent } from './components/student-detail/student-detail.component';
+import { RouterModule } from '@angular/router';
+import { StudentTopBarComponent } from './components/student-detail/student-top-bar/student-top-bar.component';
+import { StudentTabDetailComponent } from './components/student-detail/student-tab-detail/student-tab-detail.component';
 
 
 
 
 @NgModule({
   declarations: [
-    StudentListComponent
+    StudentListComponent,
+    StudentDetailComponent,
+    StudentTopBarComponent,
+    StudentTabDetailComponent
   ],
   imports: [
     CommonModule,
-    StudentRoutingModule
+    StudentRoutingModule,
+    RouterModule
   ]
 })
 export class StudentModule { }
