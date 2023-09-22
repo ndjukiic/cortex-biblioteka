9b84e76262477db722f67fd0262523dc1d442509
ndjukiic <ndjukiic@gmail.com>
2023-09-22 13:01:44 +0200

    added: form validation, error messages & submit button disable property

--- src/app/book/components/book-add/book-add-details/book-add-details.component.ts
+++ src/app/book/components/book-add/book-add-details/book-add-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Book } from 'src/app/book/models/book.model';
 import { BookService } from 'src/app/book/services/book.service';
 
@@ -14,21 +14,24 @@ export class BookAddDetailsComponent implements OnInit {
 
   ngOnInit() {
     this.bookAddForm = new FormGroup({
-      nazivKnjiga: new FormControl(null),
-      kratki_sadrzaj: new FormControl(null),
-      categories: new FormControl(null),
-      genres: new FormControl(null),
-      authors: new FormControl(null),
-      izdavac: new FormControl(null),
-      godinaIzdavanja: new FormControl(null),
-      knjigaKolicina: new FormControl(null),
-      jezik: new FormControl(1),
+      nazivKnjiga: new FormControl(null, [Validators.required]),
+      kratki_sadrzaj: new FormControl(null, [Validators.required]),
+      categories: new FormControl(null, Validators.required),
+      genres: new FormControl(null, Validators.required),
+      authors: new FormControl(null, Validators.required),
+      izdavac: new FormControl(null, Validators.required),
+      godinaIzdavanja: new FormControl(null, [Validators.required, Validators.maxLength(4)]),
+      knjigaKolicina: new FormControl(null, Validators.required),
+      jezik: new FormControl(1), 
       deletePdfs: new FormControl(0),
+      //lang and deletePdfs variables were required in api, even though they weren't in the prototype form - therefore the static content (temporarily)
     });
   }
 
   onSubmit() {
     this.storeToParent();
+    this.bookAddForm.reset();
+    
   }
 
   storeToParent(){
--- src/app/book/components/book-add/book-add-specs/book-add-specs.component.ts
+++ src/app/book/components/book-add/book-add-specs/book-add-specs.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Book } from 'src/app/book/models/book.model';
 import { BookService } from 'src/app/book/services/book.service';
 
@@ -12,22 +12,27 @@ export class BookAddSpecsComponent implements OnInit {
   @Output() formEmitter = new EventEmitter<Book>();
   bookAddForm: FormGroup;
 
-
   ngOnInit() {
     this.bookAddForm = new FormGroup({
-      brStrana: new FormControl(null),
-      pismo: new FormControl(null),
-      povez: new FormControl(null),
-      format: new FormControl(null),
-      isbn: new FormControl(null)
+      brStrana: new FormControl(null, Validators.required),
+      pismo: new FormControl(null, Validators.required),
+      povez: new FormControl(null, Validators.required),
+      format: new FormControl(null, Validators.required),
+      isbn: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(13),
+      ]),
     });
   }
 
   onSubmit() {
     this.storeToParent();
+    this.bookAddForm.reset();
   }
 
-  storeToParent(){
+  storeToParent() {
     this.formEmitter.emit(this.bookAddForm.value);
   }
+
+  isbnLength() {}
 }
