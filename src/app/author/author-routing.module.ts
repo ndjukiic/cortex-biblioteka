401e598dd1859035dd10549600da1cc6004109d0
Ida Musovic <idamusovic99@gmail.com>
2023-10-02 16:24:21 +0200

    added a routing module

---
+++ src/app/author/author-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AuthorEditComponent } from './components/author-edit/author-edit.component';
+import { AuthorDetailComponent } from './components/author-detail/author-detail.component';
+import { AuthorAddComponent } from './components/author-add/author-add.component';
+import { AuthorListComponent } from './components/author-list/author-list.component';
+
+
+const routes = [
+    {
+      path: '',
+      component: AuthorListComponent,
+    },
+    {
+      path: 'add',
+      component: AuthorAddComponent,
+    },
+    {
+      path: ':id',
+      component: AuthorDetailComponent,
+      children: [
+        {
+          path: 'details',
+          component: AuthorDetailComponent,
+        },
+    
+      ],
+    },
+    {
+      path: ':id/edit',
+      component: AuthorEditComponent,
+    },
+  ]
+  
+  @NgModule({
+    imports: [RouterModule.forChild(routes)],
+  })
+  export class AuthorRoutingModule {}
+  
