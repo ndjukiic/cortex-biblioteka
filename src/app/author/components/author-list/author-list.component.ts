a87d58d8bd370eb49724c33edf1f2ea761553318
Ida Musovic <idamusovic99@gmail.com>
2023-10-02 16:21:03 +0200

    modified app.module and app-routing.module files

--- src/app/author/components/author-list/author-list.component.ts
+++ src/app/author/components/author-list/author-list.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-author-list',
   templateUrl: './author-list.component.html',
-  styleUrls: ['./author-list.component.css']
+  styleUrls: ['./author-list.component.css'],
 })
-export class AuthorListComponent {
 
+export class AuthorListComponent {
+  lipsum =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
+
+  dummyData = [
+    {
+      name: 'Albert Kami',
+      description: this.lipsum,
+    },
+    {
+      name: 'Johan Volfgang Gete',
+      description: this.lipsum,
+    },
+    {
+      name: 'Ivo Andrić',
+      description: this.lipsum,
+    },
+    {
+      name: 'Desanka Maksimović',
+      description: this.lipsum,
+    },
+  ];
 }
