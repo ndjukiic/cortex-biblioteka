55b005a8d42585320912036721b2ed6aa06221f1
Ida Musovic <idamusovic99@gmail.com>
2023-10-02 15:37:36 +0200

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { TransactionsComponent } from './transactions/transactions.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SettingsComponent } from './settings/settings.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AuthorEditComponent } from './author/components/author-edit/author-edit.component';
+import { AuthorAddComponent } from './author/components/author-add/author-add.component';
+import { AuthorDetailComponent } from './author/components/author-detail/author-detail.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +23,9 @@ import { HttpClientModule } from '@angular/common/http';
     TransactionsComponent,
     SidebarComponent,
     SettingsComponent,
+    AuthorEditComponent,
+    AuthorAddComponent,
+    AuthorDetailComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
---
+++ src/app/author/components/author-add/author-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-author-add',
+  templateUrl: './author-add.component.html',
+  styleUrls: ['./author-add.component.css']
+})
+export class AuthorAddComponent {
+
+}
