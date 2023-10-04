8087d2543e7f33a1426d6a5b2501398ec2e33beb
Ida Musovic <idamusovic99@gmail.com>
2023-10-04 19:52:37 +0200

    updated author module

---
+++ src/app/author/author.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthorAddComponent } from './components/author-add/author-add.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthorRoutingModule } from './author-routing.module';
+import { AuthorDetailComponent } from './components/author-detail/author-detail.component';
+import { AuthorEditComponent } from './components/author-edit/author-edit.component';
+import { AuthorListComponent } from './components/author-list/author-list.component';
+import { AuthorService } from './services/author.service';
+
+
+@NgModule({
+  declarations: [
+    AuthorListComponent,
+    AuthorAddComponent,
+    AuthorDetailComponent,
+    AuthorEditComponent,
+    
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    AuthorRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    AuthorService
+  ]
+})
+export class LibrarianModule { }
--- src/app/author/services/author.service.ts
+++ src/app/author/services/author.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Author } from '../models/author.model';
+import { BehaviorSubject, Observable, Subject, map, tap } from 'rxjs';
+import { ApiResponse } from 'src/app/shared/api-response.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,6 +10,22 @@ import { Injectable } from '@angular/core';
 export class AuthorService{
 
   private url = 'https://tim7.petardev.live/api/authors';
+  private authors$ = new Subject<Author[]>();
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  loadBooks(): Observable<Author[]> {
+    return this.httpClient
+      .get<ApiResponse<Author[]>>(this.url, {
+        headers: {
+          Authorization: 'Bearer 2|DyPu5MO2VeAwQoHL8dPCkTFfXzMXkZjnP21pFxiV',
+        },
+      })
+      .pipe(
+        map((response: ApiResponse<Author[]>) => {
+          this.authors$.next(response.data);
+          return response.data;
+        })
+      );
+  }
 }
