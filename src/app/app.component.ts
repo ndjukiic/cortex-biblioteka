5d5edd5edb90ba9fc7d975931ef50b2fe11df8eb
ndjukiic <ndjukiic@gmail.com>
2023-10-11 20:50:03 +0200

    fix: sidebar now shows only if user is logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActiveUserProvider } from './auth/services/active-user.provider';
+import { AuthService } from './auth/services/auth.service';
+import { Book } from './book/models/book.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cortex-biblioteka';
+  isLoggedIn: boolean;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.user$.subscribe((response) => {
+      if (!response) {
+        this.isLoggedIn = false;
+      } else if (response) {
+        this.isLoggedIn = true;
+      }
+    });
+  }
 }
