5a86b74561c3dceee6200e23d71645cea10f3cac
Ida Musovic <idamusovic99@gmail.com>
2023-10-17 22:46:14 +0200

    updated author details top bar

--- src/app/author/components/author-detail/author-top-bar/author-top-bar.component.ts
+++ src/app/author/components/author-detail/author-top-bar/author-top-bar.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Author } from 'src/app/author/models/author.model';
+import { AuthorService } from 'src/app/author/services/author.service';
 
 @Component({
   selector: 'app-author-top-bar',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./author-top-bar.component.css']
 })
 export class AuthorTopBarComponent {
+  @Input() author: Author;
 
+  constructor(
+    private authorService: AuthorService,
+    private router: Router,
+    private route: ActivatedRoute) {}
+
+    ngOnInit() {
+      const id = +this.route.snapshot.params['id'];
+  
+      this.authorService.loadAuthor(id).subscribe((author: Author) => {
+        this.author = author;
+      });
+    }
+
+  deleteAuthor() {
+    const confirmation = window.confirm(
+      `Da li ste sigurni da želite da izbrišete korisnika ${this.author?.name} ${this.author?.surname}?`
+    );
+
+    if (confirmation) {
+      this.authorService.deleteAuthor(this.author.id).subscribe(() => {
+        this.router.navigate(['/authors']);
+      });
+    }
+  }
 }
