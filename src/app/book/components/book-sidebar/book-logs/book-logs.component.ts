42dd180867cb9aa759b0ec15a632f253804f3a9d
ndjukiic <ndjukiic@gmail.com>
2023-10-23 13:13:38 +0200

    refactor: book-logs data updates accordingly
    Component book-logs data now properly pulls active reservations and outputs data relative to the book in question.

--- src/app/book/components/book-sidebar/book-logs/book-logs.component.ts
+++ src/app/book/components/book-sidebar/book-logs/book-logs.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService } from 'src/app/book/services/book.service';
 
 @Component({
   selector: 'app-book-logs',
   templateUrl: './book-logs.component.html',
 })
-export class BookLogsComponent {
-  loop = [1, 2, 3];
+export class BookLogsComponent implements OnInit {
+  id: number;
+  activeReservations;
+
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.id = this.bookService.getBookID();
+
+    this.bookService.getActiveReservations(this.id).subscribe((response) => {
+      this.activeReservations = response.data.active.reverse().slice(0, 3);
+    });
+  }
+
+  getDaysAgo(action_date: string) {
+    const currentDate = new Date();
+    const reservationDate = new Date(action_date);
+
+    const day = currentDate.getDate() - reservationDate.getDate();
+
+    return day;
+  }
+
+  formatDate(action_date: string) {
+    const formattedDate = action_date.split(' ')[0].split('-');
+    const day = formattedDate[2];
+    const month = formattedDate[1];
+    const year = formattedDate[0];
+
+    const fullDate = `${day}.${month}.${year}`;
+
+    return fullDate;
+  }
 }
