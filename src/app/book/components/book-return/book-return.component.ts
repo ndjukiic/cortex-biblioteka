6c102b8bddb9c47d3f1071cd2b500ce2b4d02f83
Ida Musovic <ida.musovic@udg.edu.me>
2023-10-23 17:55:34 +0200

    book-return: added static data

--- src/app/book/components/book-return/book-return.component.ts
+++ src/app/book/components/book-return/book-return.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Book } from '../../models/book.model';
+import { Subscription } from 'rxjs';
+import { BookService } from '../../services/book.service';
 
 @Component({
   selector: 'app-book-return',
   templateUrl: './book-return.component.html',
 })
-export class BookReturnComponent {
+export class BookReturnComponent implements OnInit, OnDestroy {
   returnMode = true;
+  books: Book[];
+  filteredArray: Book[];
+  subscription: Subscription;
+  searchName: string;
+  sorted = false;
+  viewSize: number;
+  currentPage: number;
+
+  constructor(
+    private bookService: BookService
+  ) {}
+
+  ngOnInit() {
+    this.loadBooks();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  loadBooks() {
+    this.subscription = this.bookService
+      .loadBooks()
+      .subscribe((books: Book[]) => {
+        this.books = books;
+        this.filteredArray = this.books.slice();
+      });
+  }
 }
