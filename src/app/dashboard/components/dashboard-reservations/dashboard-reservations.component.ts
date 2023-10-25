a0636bd10673146b81c33594b8fffa3e10689068
ng1v4n85 <ng1v4n85@gmail.com>
2023-10-25 11:51:33 +0200

    feat: Added dashboard reservation

--- src/app/dashboard/components/dashboard-reservations/dashboard-reservations.component.ts
+++ src/app/dashboard/components/dashboard-reservations/dashboard-reservations.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BorrowedBook } from 'src/app/activity/models/borrowed-book.model';
+import { ActivityService } from 'src/app/activity/services/activity.service';
 
 @Component({
   selector: 'app-dashboard-reservations',
   templateUrl: './dashboard-reservations.component.html'
 })
-export class DashboardReservationsComponent {
+export class DashboardReservationsComponent implements OnInit {
 
+  activeReservations: BorrowedBook[];
+  rowsToShow: number = 4;
+  filteredActiveReservations: BorrowedBook[];
+
+  constructor(private activityService: ActivityService) {}
+
+  ngOnInit(): void {
+    this.activityService.loadActiveReservations().subscribe(
+      activeReservations => {
+        this.activeReservations = activeReservations;
+        this.filteredActiveReservations = activeReservations.slice(0, this.rowsToShow);
+      }
+    )
+  }
 }
