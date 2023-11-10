4e8582f72e0640d5329cd7f8364eb7f30662185c
ng1v4n85 <ng1v4n85@gmail.com>
2023-11-10 12:24:44 +0100

    fix: added scroll in book list

--- src/app/book/components/book-list/book-list.component.ts
+++ src/app/book/components/book-list/book-list.component.ts
@@ -7,6 +7,7 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css'],
 })
 export class BookListComponent implements OnInit, OnDestroy {
   books: Book[];
