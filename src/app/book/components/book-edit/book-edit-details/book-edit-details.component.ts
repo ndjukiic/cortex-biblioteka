c87f5d0524ea89e719708fb0b2c45b6f20b8e8dd
ndjukiic <ndjukiic@gmail.com>
2023-11-15 14:14:37 +0100

    refactor: book-edit-details form

--- src/app/book/components/book-edit/book-edit-details/book-edit-details.component.ts
+++ src/app/book/components/book-edit/book-edit-details/book-edit-details.component.ts
@@ -1,5 +1,16 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 import { Book } from 'src/app/book/models/book.model';
 import { BookService } from 'src/app/book/services/book.service';
 
@@ -11,26 +22,77 @@ declare var CKEDITOR: any;
 })
 export class BookEditDetailsComponent implements OnInit {
   @Output() formEmitter = new EventEmitter<Book>();
-  bookToEdit: Book;
   id: number;
+  bookToEdit: Book;
   bookEditForm: FormGroup;
+  isLoaded: boolean = false;
 
-  constructor(private bookService: BookService) {}
+  constructor(private bookService: BookService, private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.initEditForm();
+
     this.id = +this.bookService.getBookID();
     this.bookService.loadBookForEdit(this.id).subscribe((response) => {
       this.bookToEdit = response;
+      this.patchEditForm();
     });
 
     setTimeout(() => {
-      this.initForm();
       this.loadCKEditor();
     }, 500);
   }
 
+  initEditForm() {
+    this.bookEditForm = this.fb.group({
+      nazivKnjiga: ['', [Validators.required]],
+      kratki_sadrzaj: [''],
+      categories: ['', [Validators.required]],
+      genres: ['', [Validators.required]],
+      authors: ['', [Validators.required]],
+      izdavac: ['', [Validators.required]],
+      godinaIzdavanja: ['', [Validators.required, Validators.maxLength(4)]],
+      knjigaKolicina: ['', [Validators.required, this.greaterThanZero]],
+    });
+  }
+
+  patchEditForm() {
+    this.bookEditForm.patchValue({
+      nazivKnjiga: this.bookToEdit.book.title,
+      kratki_sadrzaj: this.bookToEdit.book.description,
+      categories: this.bookToEdit.book.categories,
+      genres: this.bookToEdit.book.genres,
+      authors: this.bookToEdit.book.authors,
+      izdavac: this.bookToEdit.book.publisher,
+      godinaIzdavanja: this.bookToEdit.book.pDate,
+      knjigaKolicina: this.bookToEdit.book.quantity,
+    });
+    this.isLoaded = true;
+  }
+
   loadCKEditor() {
     CKEDITOR.replace('content');
+  }
+
+  greaterThanZero(control: FormControl): {
+    [validation: string]: boolean;
+  } {
+    const value = control.value;
+    if (value < 0) {
+      return { greaterThanZero: false };
+    }
+    return null;
+  }
+
+  onSubmit() {
+    const content = this.getDataFromCKEditor();
+    this.bookEditForm.get('kratki_sadrzaj').setValue(content);
+    console.log(this.bookEditForm.value);
+    this.storeToParent();
+  }
+
+  storeToParent() {
+    this.formEmitter.emit(this.bookEditForm.value);
   }
 
   getDataFromCKEditor() {
@@ -41,30 +103,5 @@ export class BookEditDetailsComponent implements OnInit {
     const data = editor.getData();
     console.log(data, 'ckeditor');
     return data;
-  }
-
-  initForm() {
-    this.bookEditForm = new FormGroup({
-      nazivKnjiga: new FormControl(this.bookToEdit.book.title),
-      kratki_sadrzaj: new FormControl(this.bookToEdit.book.description),
-      categories: new FormControl(this.bookToEdit.book.categories),
-      genres: new FormControl(this.bookToEdit.book.genres),
-      authors: new FormControl(this.bookToEdit.book.authors),
-      izdavac: new FormControl(this.bookToEdit.book.publisher.id),
-      godinaIzdavanja: new FormControl(this.bookToEdit.book.pDate),
-      knjigaKolicina: new FormControl(this.bookToEdit.book.samples),
-      jezik: new FormControl(1),
-      deletePdfs: new FormControl(0),
-    });
-  }
-
-  onSubmit() {
-    const content = this.getDataFromCKEditor();
-    this.bookEditForm.get('kratki_sadrzaj').setValue(content);
-    this.storeToParent();
-  }
-
-  storeToParent() {
-    this.formEmitter.emit(this.bookEditForm.value);
   }
 }
